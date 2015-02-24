d38e92dedca8c5b12b3f7d2cc5d618643114750b
Dillon Lee <dillonlee@live.co.uk>
2015-02-24 15:38:03 +0000

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ersimulationToolApp
+ * @description
+ * # ersimulationToolApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ersimulationToolApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
